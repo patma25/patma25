08b697e1a5e00a8f27ce74841c3f7de303a7366d
patma25 <patmajanuari@gmail.com>
2025-03-03 14:01:14 +0800

    Update Statistik.py

--- Statistik.py
+++ Statistik.py
@@ -20,8 +20,3 @@ Data: [5, 3, 5, 8, 9, 3, 5, 7, 6 ,3]
 mean (rata-rata): 5.4
 median: 5.0
 modus: 3
-
-<!---
-patma25/patma25 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
