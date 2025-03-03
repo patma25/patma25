831fba3aed93a189f7c346cb62490316a74de0ca
patma25 <patmajanuari@gmail.com>
2025-03-03 13:59:29 +0800

    Create Statistik.py

---
+++ Statistik.py
@@ -0,0 +1,27 @@
+nama="pasrawan patma"
+nim ="D0424302"
+
+import statistics
+#Data
+data = [5, 3, 5, 8, 9, 3, 5, 7, 6,]
+
+#mean (rata-rata)
+mean = statistics.mean(data)
+
+#modus = statistics.mode(data)
+
+#output
+print(f"Data: {data}")
+print(f"Mean (rata-rata): {mean}")
+print(f"median: {median}")
+print(f"modus: {modus}")
+
+Data: [5, 3, 5, 8, 9, 3, 5, 7, 6 ,3]
+mean (rata-rata): 5.4
+median: 5.0
+modus: 3
+
+<!---
+patma25/patma25 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
